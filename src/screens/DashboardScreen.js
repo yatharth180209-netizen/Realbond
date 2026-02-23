dae4d7be7d87d386691af8ab7128f8f9ef7e8559
yatharth180209-netizen <yatharth180209@gmail.com>
2026-02-23 11:11:00 +0530

    Create DashboardScreen.js for RealBond dashboard

---
+++ src/screens/DashboardScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardScreen = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Welcome to the RealBond Dashboard!</p>
+        </div>
+    );
+};
+
+export default DashboardScreen;
